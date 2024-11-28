cb8b0dbb31dbfef04e9c3767a8d5c96c6ca93f22
GuilleMarejada <guillermo.gil.cor@gmail.com>
2024-11-28 20:12:59 +0000

    arreglo TS en astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 // @ts-check
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from "@astrojs/vercel/serverless";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
-  output: 'server',
-  adapter: vercel()
+  output: "server",
+  adapter: vercel({}),
 });
