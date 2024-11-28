afb3690d75c2fc58015210c03fa16c586434fbb7
GuilleMarejada <guillermo.gil.cor@gmail.com>
2024-11-28 19:20:05 +0000

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
+import vercel from '@astrojs/vercel/serverless';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), sitemap()],
+  output: 'server',
+  adapter: vercel()
+});
